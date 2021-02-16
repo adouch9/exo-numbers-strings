69053060e1c777253f347899d9861a0833be302a
adouch9 <mr.ouldmoussa@gmail.com>
2021-02-16 16:16:30 +0100

    showChar

---
+++ showChar.js
@@ -0,0 +1,11 @@
+
+
+let msg = `C'était à Mégara, faubourg de Carthage, dans les jardins d'Hamilcar.`
+
+for (let i = 0; i < msg.length; i++) {
+  if (msg[i] !== ' ') {
+    console.log(`${msg[i]} is at index ${i}`)
+  }
+};
+
+
